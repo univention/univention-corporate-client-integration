ba931b41684833132800280926563e353233af67
Moritz Mühlenhoff <muehlenhoff@univention.de>
2013-01-10 13:55:04 +0000

    Add support for management of UCC multi monitor settings (Bug #28315)
    git-svn-id: svn+ssh://billy.knut.univention.de/var/univention/svn/dev/branches/ucs-3.1/ucc-integration@38198 1b283449-9f2f-0410-b571-8eb228e1a901

--- ucc-management-integration/modules/policies/ucc_hardware.py
+++ ucc-management-integration/modules/policies/ucc_hardware.py
@@ -84,6 +84,57 @@ property_descriptions={
 			may_change=1,
 			identifies=0
 		),
+	'prim_res': univention.admin.property(
+			short_description=_('Resolution of primary display'),
+			long_description=_('The resolution in pixels (XxY) of the primary display, if left blank the detection is done automatically'),
+			syntax=univention.admin.syntax.XResolution,
+			multivalue=0,
+			options=[],
+			required=0,
+			may_change=1,
+			identifies=0
+		),
+	'sec_res': univention.admin.property(
+			short_description=_('Resolution of secondary display'),
+			long_description=_('The resolution in pixels (XxY) of the secondary display, if left blank the detection is done automatically'),
+			syntax=univention.admin.syntax.XResolution,
+			multivalue=0,
+			options=[],
+			required=0,
+			may_change=1,
+			identifies=0
+		),
+	'prim_name': univention.admin.property(
+			short_description=_('Name of primary display'),
+			long_description=_('The X name of the primary display device (can be queried with xrandr --list)'),
+			syntax=univention.admin.syntax.string,
+			multivalue=0,
+			options=[],
+			required=0,
+			may_change=1,
+			identifies=0
+		),
+	'sec_name': univention.admin.property(
+			short_description=_('Name of secondary display'),
+			long_description=_('The X name of the secondary display device (can be queried with xrandr --list)'),
+			syntax=univention.admin.syntax.string,
+			multivalue=0,
+			options=[],
+			required=0,
+			may_change=1,
+			identifies=0
+		),
+	'display-position': univention.admin.property(
+			short_description=_('Position of secondary display relative to the primary'),
+			long_description='',
+			syntax=univention.admin.syntax.XDisplayPosition,
+			multivalue=0,
+			options=[],
+			required=0,
+			may_change=1,
+			identifies=0,
+			default=('', [])
+		),
 	'requiredObjectClasses': univention.admin.property(
 			short_description=_('Required object classes'),
 			long_description='',
@@ -136,10 +187,17 @@ property_descriptions={
 		)
 }
 layout = [
-	Tab(_('General'),_('UCC user session'), layout = [
+	Tab(_('General'),_('UCC client configuration'), layout = [
 		Group( _( 'General' ), layout = [
 			'name',
+		] ),
+		Group( _( 'Mass storage' ), layout = [
 			'massstorage',
+		] ),
+		Group( _( 'Multi monitor configuration' ), layout = [
+			[ 'prim_res', 'sec_res' ], 
+			[ 'prim_name', 'sec_name' ], 
+			'display-position',
 		] ),
 	] ),
 	Tab(_('Object'),_('Object'), advanced = True, layout = [
@@ -151,6 +209,13 @@ layout = [
 mapping=univention.admin.mapping.mapping()
 mapping.register('name', 'cn', None, univention.admin.mapping.ListToString)
 mapping.register('massstorage', 'univentionCorporateClientComputerLocalStorage', None, univention.admin.mapping.ListToString)
+mapping.register('prim_res', 'univentionCorporateClientPrimaryDisplayResolution', None, univention.admin.mapping.ListToString)
+mapping.register('sec_res', 'univentionCorporateClientSecondaryDisplayResolution', None, univention.admin.mapping.ListToString)
+mapping.register('prim_name', 'univentionCorporateClientPrimaryDisplayName', None, univention.admin.mapping.ListToString)
+mapping.register('sec_name', 'univentionCorporateClientSecondaryDisplayName', None, univention.admin.mapping.ListToString)
+mapping.register('display-position', 'univentionCorporateClientDisplayPosition', None, univention.admin.mapping.ListToString)
+
+
 
 class object(univention.admin.handlers.simplePolicy):
 	module=module
@@ -193,4 +258,4 @@ def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', unique=0,
 	return res
 
 def identify(dn, attr, canonical=0):
-	return 'univentionCorporateClientComputerLocalStorage' in attr.get('objectClass', [])
+	return 'univentionPolicyCorporateClientComputer' in attr.get('objectClass', [])
